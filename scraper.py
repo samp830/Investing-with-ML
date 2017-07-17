e610ed833fe45db8569106568eaf6153e6981044
Samyak Parajuli <sparajuli@Samyaks-MacBook-Pro.local>
2017-07-17 00:31:04 -0700

    Multi-threading scraper

--- scraper.py
+++ scraper.py
@@ -6,6 +6,8 @@ import re
 import json
 import requests
 import os
+from threading import Thread
+
 
 symbols = ['ABT', 'ABBV', 'ACN', 'ACE', 'ADBE', 'ADT', 'AAP', 'AES', 'AET', 'AFL', 'AMG', 'A', 'GAS', 'APD', 'ARG'
           ,'AKAM', 'AA', 'AGN', 'ALXN', 'ALLE', 'ADS', 'ALL', 'ALTR', 'MO', 'AMZN', 'AEE', 'AAL', 'AEP', 'AXP', 'AIG', 'AMT', 'AMP', 'ABC',
@@ -19,18 +21,44 @@ symbols = ['ABT', 'ABBV', 'ACN', 'ACE', 'ADBE', 'ADT', 'AAP', 'AES', 'AET', 'AFL
           'LVLT', 'LUK', 'LLY', 'LNC', 'LLTC', 'LMT', 'L', 'LOW', 'LYB', 'MTB', 'MAC', 'M', 'MNK', 'MRO', 'MPC', 'MAR', 'MMC', 'MLM', 'MAS', 'MA', 'MAT', 'MKC', 'MCD', 'MHFI', 'MCK', 'MJN', 'MMV', 'MDT', 'MRK', 'MET', 'KORS', 'MCHP', 'MU', 'MSFT', 'MHK', 'TAP', 'MDLZ', 'MON', 'MNST', 'MCO', 'MS', 'MOS', 'MSI', 'MUR', 'MYL', 'NDAQ', 'NOV', 'NAVI', 'NTAP', 'NFLX', 'NWL', 'NFX', 'NEM', 'NWSA', 'NEE', 'NLSN', 'NKE', 'NI', 'NE', 'NBL', 'JWN', 'NSC', 'NTRS', 'NOC', 'NRG', 'NUE', 'NVDA', 'ORLY', 'OXY', 'OMC', 'OKE', 'ORCL', 'OI', 'PCAR', 'PLL', 'PH', 'PDCO', 'PAYX', 'PNR', 'PBCT', 'POM', 'PEP', 'PKI', 'PRGO', 'PFE', 'PCG', 'PM', 'PSX', 'PNW', 'PXD', 'PBI', 'PCL', 'PNC', 'RL', 'PPG', 'PPL', 'PX', 'PCP', 'PCLN', 'PFG', 'PG', 'PGR', 'PLD', 'PRU', 'PEG', 'PSA', 'PHM', 'PVH', 'QRVO', 'PWR', 'QCOM', 'DGX', 'RRC', 'RTN', 'O', 'RHT', 'REGN', 'RF', 'RSG', 'RAI', 'RHI', 'ROK', 'COL', 'ROP', 'ROST', 'RLC', 'R', 'CRM', 'SNDK', 'SCG', 'SLB', 'SNI', 'STX', 'SEE', 'SRE', 'SHW', 'SIAL', 'SPG', 'SWKS', 'SLG', 'SJM', 'SNA', 'SO', 'LUV', 'SWN', 'SE', 'STJ', 'SWK', 'SPLS', 'SBUX', 'HOT', 'STT', 'SRCL', 'SYK', 'STI', 'SYMC', 'SYY', 'TROW', 'TGT', 'TEL', 'TE', 'TGNA', 'THC', 'TDC', 'TSO', 'TXN', 'TXT', 'HSY', 'TRV', 'TMO', 'TIF', 'TWX', 'TWC', 'TJK', 'TMK', 'TSS', 'TSCO', 'RIG', 'TRIP', 'FOXA', 'TSN', 'TYC', 'UA', 'UNP', 'UNH', 'UPS', 'URI', 'UTX', 'UHS', 'UNM', 'URBN', 'VFC', 'VLO', 'VAR', 'VTR', 'VRSN', 'VZ', 'VRTX', 'VIAB', 'V', 'VNO', 'VMC', 'WMT', 'WBA', 'DIS', 'WM', 'WAT', 'ANTM', 'WFC', 'WDC', 'WU', 'WY', 'WHR', 'WFM', 'WMB', 'WEC', 'WYN', 'WYNN', 'XEL', 'XRX', 'XLNX', 'XL', 'XYL', 'YHOO', 'YUM', 'ZBH', 'ZION', 'ZTS']
 
 
-for symbol in symbols:
-    script_dir = os.path.dirname(__file__)
-    print (script_dir)
-    rel_path = "daily_prices/"
-    abs_file_path = os.path.join(script_dir, rel_path)
-    print("-------------", abs_file_path)
-    myfile = open(abs_file_path + symbol + ".txt", "w+")
+# for symbol in symbols:
+#     script_dir = os.path.dirname(__file__)
+#     print (script_dir)
+#     rel_path = "daily_prices/"
+#     abs_file_path = os.path.join(script_dir, rel_path)
+#     print("-------------", abs_file_path)
+#     myfile = open(abs_file_path + symbol + ".txt", "w+")
+#     myfile.close()
+#     url = "https://www.bloomberg.com/markets/api/bulk-time-series/price/"+symbol+"%3AUS?timeFrame=1_DAY"
+#     rawdata = urllib.request.urlopen(url).read()
+#     jsonData = json.loads(rawdata.decode('utf-8'))
+#     myfile = open(abs_file_path + symbol + ".txt", "a")
+#     for element in jsonData[0]['price']:
+#         myfile.write(str(symbol) + "," + str(element["dateTime"]) + "," + str(element["value"]) + "\n")
+#     myfile.close()
+
+# script_dir = os.path.dirname('__file__')
+# print (script_dir)
+# rel_path = "daily_prices/"
+# abs_file_path = os.path.join(script_dir, re)
+
+def th(url):
+    myfile = open("/Users/sparajuli/Desktop/UCB/fun/Stock_SVM/daily_prices/" + symbol + ".txt", "w+")
     myfile.close()
-    url = "https://www.bloomberg.com/markets/api/bulk-time-series/price/"+symbol+"%3AUS?timeFrame=1_DAY"
     rawdata = urllib.request.urlopen(url).read()
     jsonData = json.loads(rawdata.decode('utf-8'))
-    myfile = open(abs_file_path + symbol + ".txt", "a")
+    myfile = open("/Users/sparajuli/Desktop/UCB/fun/Stock_SVM/daily_prices/" + symbol + ".txt", "a")
     for element in jsonData[0]['price']:
         myfile.write(str(symbol) + "," + str(element["dateTime"]) + "," + str(element["value"]) + "\n")
     myfile.close()
+
+threadlist = []
+
+for symbol in symbols:
+    url = "https://www.bloomberg.com/markets/api/bulk-time-series/price/"+symbol+"%3AUS?timeFrame=1_DAY"
+    t = Thread(target =th, args = (url,))
+    t.start()
+    threadlist.append(t)
+    
+for b in threadlist:
+    b.join()
